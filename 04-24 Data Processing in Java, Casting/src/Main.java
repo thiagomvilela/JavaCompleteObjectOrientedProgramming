148f902e11d1de4460061ecbaad170248e0e1693
Thiago Vilela <thiago.vilela@outlook.com.br>
2022-12-30 14:16:19 -0300

    Update New Files

---
+++ 04-24 Data Processing in Java, Casting/src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Exemplo 1
+        /*
+        int x, y;
+        x = 5;
+        y = 2 * x;
+        System.out.println(x);
+        System.out.println(y);
+        */
+
+        // Exemplo 2
+        /*
+        int x;
+        double y;
+        x = 5;
+        y = 2 * x;
+        System.out.println(x);
+        System.out.println(y);
+        */
+
+        // Exemplo 3
+        /*
+        double b, B, h, area;
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+        area = (b + B) / 2.0 * h;
+        System.out.println(area);
+        */
+
+        // Exemplo 4
+        /*
+        int a, b;
+        double resultado;
+        a = 5;
+        b = 2;
+        //resultado = a / b;
+        resultado = (double) a / b; // Casting
+        System.out.println(resultado);
+        */
+
+        // Exemplo 5
+
+        double a;
+        int b;
+        a = 5.0;
+        // b = a;
+        b = (int) a; // Casting
+        System.out.println(b);
+
+    }
+}
