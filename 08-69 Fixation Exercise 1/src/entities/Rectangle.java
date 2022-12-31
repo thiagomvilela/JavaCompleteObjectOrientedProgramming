fa6c636f642991457bb5255e74d988d5f9eee945
Thiago Vilela <thiago.vilela@outlook.com.br>
2022-12-31 20:06:33 -0300

    Updade New Files

---
+++ 08-69 Fixation Exercise 1/src/entities/Rectangle.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class Rectangle {
+	
+	// Attributes
+	
+	public double width;
+	public double height;
+	
+	// Functions
+	
+	public double area() {
+		return width * height;
+	}
+	
+	public double perimeter() {
+		return (width + height) * 2.0;
+	}
+	
+	public double diagonal() {
+		return Math.sqrt(width * width + height * height);
+	}
+	
+}
