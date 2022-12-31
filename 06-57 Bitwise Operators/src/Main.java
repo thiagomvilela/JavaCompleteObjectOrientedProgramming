60415c5c4d76084ebcf0d3738a959c634d8d7806
Thiago Vilela <thiago.vilela@outlook.com.br>
2022-12-31 09:48:29 -0300

    Upload New Files

---
+++ 06-57 Bitwise Operators/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*
+		int n1 = 89;
+		int n2 = 60;
+		System.out.println(n1 & n2); // 24
+		System.out.println(n1 | n2); // 125
+		System.out.println(n1 ^ n2); // 101
+		*/
+		
+		Scanner sc = new Scanner(System.in);
+
+		int mask = 0b100000; // 32
+
+		int n = sc.nextInt();
+
+		if ((n & mask) != 0) {
+			System.out.println("6th bit is true!");
+			
+		} else {
+			System.out.println("6th bit is false");
+			
+		}
+		sc.close();
+	}
+
+}
